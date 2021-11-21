d23b6aeb79d71e802796a8c0160955ed4a6c1eec
Bruno-Mafra <74402232+Bruno-Mafra@users.noreply.github.com>
2021-11-21 06:57:52 -0800

    Add files via upload

---
+++ Código-Fonte/greenredlight2/paredes.hpp
@@ -0,0 +1,39 @@
+#ifndef PAREDES_HPP_
+#define PAREDES_HPP_
+
+#include <vector>
+
+#include "abcg.hpp"
+#include "camera.hpp"
+
+struct Vertex2 {
+  glm::vec3 position{};
+
+  bool operator==(const Vertex2& other) const noexcept {
+    return position == other.position;
+  }
+};
+
+class Paredes {
+ public:
+  void createBuffers(GLuint program);
+  void loadObj(std::string_view path, bool standardize = true);
+  void render(Camera m_camera);
+  //void setupVAO(GLuint program);
+  void terminateGL();
+
+ private:
+  GLuint m_VAO{};
+  GLuint m_VBO{};
+  GLuint m_EBO{};
+  GLuint m_program{};
+
+  glm::vec4 color { 0/255.0f, (191.0/255.0)*2.0f, (255.0/255.0f)*2.0f, 1.0f}; // *2 pq essa é a cor de trás dos triangulos
+
+  std::vector<Vertex2> m_vertices;
+  std::vector<GLuint> m_indices;
+
+  void standardize();
+};
+
+#endif
